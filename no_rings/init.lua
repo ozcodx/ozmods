278a5a7c2ba743720bab173f964b5e3890f564a2
Oz <ozcodx@gmail.com>
2024-11-20 23:29:25 -0500

    no_rings complete functionality

--- no_rings/init.lua
+++ no_rings/init.lua
@@ -1,34 +1,13 @@
-
-core.debug("Hello?")
-
-gens = core.registered_on_generateds
-
-for key, value in pairs(gens) do
-    minetest.debug(key .. ": " .. tostring(value))
+function remove_rings()
+    for index, func in pairs(core.registered_on_generateds) do
+        local info = debug.getinfo(func)
+        local source = info.source
+        local modname = string.match(source, ".+/([^/]+)/[^/]+$")
+        if modname == "rings" then
+            table.remove(core.registered_on_generateds, index)
+            core.debug("function removed by remove_rings")
+        end
+    end
 end
 
-core.debug("Info 2")
-targetfun = gens[2]
-
-info = debug.getinfo(targetfun)
-
-for key, value in pairs(info) do
-    minetest.debug(key .. ": " .. tostring(value))
-end
-
-targetsrc = info.source
-
-local targetmodname = string.match(targetsrc, ".+/([^/]+)/[^/]+$")
-
-core.debug("modname: ".. targetmodname)
-
-table.remove(core.registered_on_generateds, 2)
-
-
-core.debug("Hello Again!")
-
-gens = core.registered_on_generateds
-
-for key, value in pairs(gens) do
-    minetest.debug(key .. ": " .. tostring(value))
-end
+remove_rings()
